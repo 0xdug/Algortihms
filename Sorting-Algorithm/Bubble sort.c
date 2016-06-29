c0ad6a7e9ff68d1b96491637c3152353172d28fc
durga94 <durgasubramanian94@gmail.com>
2016-06-29 20:58:34 +0530

    Create Bubble sort.c

---
+++ Sorting-Algorithm/Bubble sort.c
@@ -0,0 +1,16 @@
+/** Bubble Sort Algorithm
+
+*begin BubbleSort(list)
+*	for all elements of list
+*		if list[i] > list[i+1]
+*			swap(list[i], list[i+1])
+*		end if
+*	end for
+*	return list
+*/ end BubbleSort
+
+/* C program to sort data in ascending order */
+
+#include<stdio.h>
+int main() {
+	
