97c4bf10bf447f0c4be3e85f6ce9f05f56c019f1
durga94 <durgasubramanian94@gmail.com>
2016-06-29 21:36:22 +0530

    Update Bubble sort.c

--- Sorting-Algorithm/Bubble sort.c
+++ Sorting-Algorithm/Bubble sort.c
@@ -13,4 +13,25 @@
 
 #include<stdio.h>
 int main() {
-	
+	int array[100], size, i, j, temp;
+	printf("Enter the number of elements:");
+	scanf("%d", &size);
+	printf("Enter the elements:");
+	for(i=0;i<size;i++){
+		scanf("%d",&array[i]);
+	}
+	for(i=0;i<size;i++){
+		for(j=0;j<size-i-1;j++){
+			if(array[i]>array[i+1]){
+				temp=array[i];
+				array[i]=array[i+1];
+				array[i+1]=temp;
+			}
+		}
+	}
+	printf("Sorted Array\n");
+	for(i=0;i<size;i++){
+		printf("%d\n",array[i]);	
+	}
+	return 0;
+}
