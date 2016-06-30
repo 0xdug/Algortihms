43466663148861c68782ce7465a80ba85ae96f87
durga94 <durgasubramanian94@gmail.com>
2016-06-30 17:08:06 +0530

    Create Selection_Sort.c

---
+++ Sorting-Algorithm/Selection_Sort.c
@@ -0,0 +1,10 @@
+/** Selection sort
+* Set min to zeroth index
+* Search the minimum element in the list
+* Swap with value at location min
+* Increment min to point to next element
+* Repeat until the list is sorted
+**/
+
+/* Program to sort array using selection sort */
+
