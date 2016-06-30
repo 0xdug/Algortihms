d3e634eff0d4f4c98e1a5542ae207af91047949c
durga94 <durgasubramanian94@gmail.com>
2016-06-30 17:24:22 +0530

    Update Selection_Sort.c

--- Sorting-Algorithm/Selection_Sort.c
+++ Sorting-Algorithm/Selection_Sort.c
@@ -8,3 +8,27 @@
 
 /* Program to sort array using selection sort */
 
+#include<stdio.h>
+int main() {
+	int array[100], size, i, j, temp;
+	printf("Enter the number of elements:");
+	scanf("%d", &size);
+	printf("Enter the elements:");
+	for(i=0;i<size;i++){
+		scanf("%d",&array[i]);
+	}
+	for(i=0;i<size;i++) {
+		for(j=i+1;j<size;j++){
+			if(array[j]<array[i]){
+				temp=array[i];
+				array[i]=array[j];
+				array[j]=temp;
+			}
+		}
+	}
+	printf("Sorted Array\n");
+	for(i=0;i<size;i++){
+		printf("%d\n",array[i]);	
+	}
+	return 0;
+}
